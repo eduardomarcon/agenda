76bc66b3bcfb49c76f9571e343212a810b538a09
eduardomarcon <eduardommarcon@gmail.com>
2024-08-24 17:16:00 -0300

    feat: adding home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Calendar, Container, Hero } from '@/pages/home/styles'
+
+import calendarImage from '../../assets/calendar.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Easier schedule
+        </Heading>
+        <Text size="xl">
+          Connect your calendar and let people schedule a meeting with you
+        </Text>
+      </Hero>
+
+      <Calendar>
+        <Image
+          src={calendarImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Calendar>
+    </Container>
+  )
+}
