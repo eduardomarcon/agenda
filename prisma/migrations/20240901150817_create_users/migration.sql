4dd4c3dd31b3a43a124d1cc4ff97944c5829018d
eduardomarcon <eduardommarcon@gmail.com>
2024-09-01 17:07:12 -0300

    feat: changing database for mysql

---
+++ prisma/migrations/20240901150817_create_users/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `fullname` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `avatar_url` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
