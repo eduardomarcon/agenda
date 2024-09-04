cf470f26521e7b46e4cdc6a94527000159538c5d
eduardomarcon <eduardommarcon@gmail.com>
2024-09-03 22:34:15 -0300

    feat: adding option to get a short weekday

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,16 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekDay) =>
-      weekDay.substring(0, 1).toLowerCase().concat(weekDay.substring(1)),
-    )
+    .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toLowerCase()
+      }
+      return weekDay.substring(0, 1).toLowerCase().concat(weekDay.substring(1))
+    })
 }
